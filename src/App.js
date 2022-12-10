39615b888f10b890918b33b6e1167778b6ea60cd
deepanshumidha5140 <110282117+deepanshumidha5140@users.noreply.github.com>
2022-12-10 18:05:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import './Page.js';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Area he visited yesterday for marketing.
+        </p>
+        <input className='text' type='text' placeholder='' />
+        <div>
+      <a href="Page">
+        <button>Next</button>
+      </a>
+    </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Page.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="Page">
+      <header className="Page">
+        <p>Page2</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
